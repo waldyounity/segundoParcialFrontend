1a8ee45b7f480128ca17071930223d8980fbb81d
waldyounity <waldhur@gmail.com>
2025-10-27 21:12:51 -0400

    Se creo el proyecto en angular y los componentes necesarios

---
+++ src/app/layout/layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css',
+})
+export class Layout {
+
+}
